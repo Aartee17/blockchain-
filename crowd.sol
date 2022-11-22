e3312c4528f4198a92a2fc0f5d9e7bbcbfbb2084
Aartee17 <93087961+Aartee17@users.noreply.github.com>
2022-11-22 15:46:35 +0530

    Create crowd.sol

---
+++ crowd.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// Aartee chimate
+// 2018140012 BEIT
+contract crowdfunding {
+    mapping(address => uint) contributors;
+    address public manager;
+    uint public minimumContribution;
+    uint public deadline;
+    uint public target;
+    uint public raisedAmount;
+    uint public noOfContributors;
+
+    struct Request {
+        string eventName;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address => bool) voters;
+    }
+    mapping(uint => Request) public requests;
+    uint public numRequests;
+
+    constructor(uint _target, uint _deadline) {
+        target = _target;
+        deadline = block.timestamp + _deadline;
+        minimumContribution = 100 wei;
+        manager = msg.sender;
+    }
+
+    function sendEth() public payable {
+        require(manager != msg.sender, "Manager Cannot contribute here");
+        require(block.timestamp < deadline, "Deadline has passed");
+        require(
+            msg.value >= minimumContribution,
+            "Minimum Contribution is not met"
+        );
+
+        if (contributors[msg.sender] == 0) {
+            noOfContributors++;
+        }
+        contributors[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+    }
+
+    function getContractBal() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function refundAmount() public payable {
+        require(
+            block.timestamp > deadline && raisedAmount < target,
+            "You are not eligible for refund"
+        );
+        require(contributors[msg.sender] > 0);
+        address payable user = payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender] = 0;
+    }
+    
+    modifier onlyManager{
+         require(manager != msg.sender, "Manager Cannot contribute here");
+         _;
+    }
+
+    function createRequest(
+        string memory _description,
+        address payable _recipient,
+        uint _value
+    ) public onlyManager {
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        newRequest.eventName = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
+    }
+    function voteRequest(uint _requestNo) public {
+        require(contributors[msg.sender]>0, "Y0u must be contributor");
+        Request storage thisRequest=requests[_requestNo];
+        require(thisRequest.completed==false, "The request has been completed");
+        require(thisRequest.noOfVoters > noOfContributors/2, "Majority does not support");
+        thisRequest.voters[msg.sender]=true;
+        thisRequest.noOfVoters++;
+    }
+    function makepayment(uint _requestNo) public onlyManager{
+        require(raisedAmount>=target);
+        Request storage thisRequest=requests[_requestNo];
+        require(thisRequest.completed==false, "The request has been completed");
+        require(thisRequest.noOfVoters > noOfContributors/2, "Majority does not support");
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed=true;
+
+    }
+}
